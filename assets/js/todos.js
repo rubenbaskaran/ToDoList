60a770ef69f9b099ef10444dbf28f6272d08ecc2
Ruben Baskaran <rbaskaran92@gmail.com>
2020-02-18 21:29:20 +0100

    added fadeout and removal to li

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -2,3 +2,10 @@ $("li").click(function ()
 {
     $(this).toggleClass("completed");
 });
+
+$("span").click(function(event){
+    $(this).parent().fadeOut(500, function(){
+        $(this).remove();
+    });
+    event.stopPropagation();
+});
