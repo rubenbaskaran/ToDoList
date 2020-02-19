588dc6400435fe55bd89f30760197e22e71c68fa
Ruben Baskaran <rbaskaran92@gmail.com>
2020-02-20 00:17:25 +0100

    added plus sign toggle feature

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -21,3 +21,7 @@ $("input[type='text']").keypress(function (event)
         $("ul").append("<li><span><i class='fa fa-trash'></i></span>" + input + "</li>");
     }
 });
+
+$(".fa-plus").click(function(){
+    $("input[type='text']").fadeToggle();
+});
