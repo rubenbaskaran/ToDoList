de61ad3a0b6607c947baa94099932656341f072f
Ruben Baskaran <rbaskaran92@gmail.com>
2020-02-19 23:35:17 +0100

    added trash icon

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -18,6 +18,6 @@ $("input[type='text']").keypress(function (event)
     {
         var input = $(this).val();
         $(this).val("");
-        $("ul").append("<li><span>X </span>" + input + "</li>");
+        $("ul").append("<li><span><i class='fa fa-trash'></i></span>" + input + "</li>");
     }
 });
