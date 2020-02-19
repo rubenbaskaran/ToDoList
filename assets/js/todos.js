80e209dc3ec8791abc39d75eef01764a4c63df6a
Ruben Baskaran <rbaskaran92@gmail.com>
2020-02-19 17:01:43 +0100

    added code for appending new list items to the todo list

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -1,11 +1,23 @@
-$("li").click(function ()
+$("ul").on("click", "li", function ()
 {
     $(this).toggleClass("completed");
 });
 
-$("span").click(function(event){
-    $(this).parent().fadeOut(500, function(){
+$("ul").on("click", "span", function (event)
+{
+    $(this).parent().fadeOut(500, function ()
+    {
         $(this).remove();
     });
     event.stopPropagation();
+});
+
+$("input[type='text']").keypress(function (event)
+{
+    if (event.which === 13)
+    {
+        var input = $(this).val();
+        $(this).val("");
+        $("ul").append("<li><span>X </span>" + input + "</li>");
+    }
 });
